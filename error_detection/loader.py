241f85985e919c84dc9f8cc3c0fd07f82249898e
NoahMacRitchie <noah21mac@gmail.com>
2021-02-07 20:40:46 -0800

    added options.py functionality to loader.py | can now encode straight to a usb

--- error_detection/loader.py
+++ error_detection/loader.py
@@ -1,22 +1,15 @@
-from cryptography.fernet import Fernet
 import os
-def load_key(key_file):
-    return open(key_file, "rb").read()
+import argparse
+from cryptography.fernet import Fernet
+from common import load_key, get_usb_devices, get_mount_points
 
-def generate_key():
-    key = Fernet.generate_key()
-    with open("./test.key", "wb") as key_file:
-        key_file.write(key)
-
-
-def encrypt_data(filename):
+def encrypt_data(filename, keyfile, output_path):
 
     with open(filename, 'r') as file:
         data = file.read()
 
-    print(data);
     msg = data.encode()
-    keyfile = "testing.key"
+
     key = load_key(keyfile)
     f = Fernet(key)
 
@@ -25,7 +18,53 @@ def encrypt_data(filename):
     encrypt_output_file = open("encrypt_output.txt", "wb")
     encrypt_output_file.write(encrypted_msg)
     encrypt_output_file.close()
-    os.system('./encode-crc32 encrypt_output.txt > encoded_data.txt')
+    os.system('./encode-crc32 encrypt_output.txt > {}encoded_data.txt'.format(output_path))
 
 
-encrypt_data("test.json")
+# parsing arguments
+parser = argparse.ArgumentParser(description='''mode is an integer,
+ 1:usb \n                                                                    
+ 2:specific path \n                       
+ 3:local folder\n''')
+
+# exporting to an usb?              1
+#           to a specific path?     2
+#           to local folder?        3
+parser.add_argument(
+'mode',
+type=int,
+help='''mode is an integer,
+ 1: usb, will return a list of usb devices and prompts for futher inputs. \n                                                                    
+ 2: specific path, followed by -d <path> or --dir <path>                      
+ 3: local folder\n''')
+
+parser.add_argument('-d', '--dir')
+
+args = parser.parse_args()
+
+print(args)
+
+# to usb
+if(args.mode == 1):
+    mount_points = get_mount_points()
+    print(mount_points)
+    usb_choice = input("input the index of the usb array(starting from 0):")
+    dir = mount_points[int(usb_choice)][1] + "/"
+
+
+# to a path
+elif(args.mode ==2):
+    #grabs the --dir argument.
+    dir = args.dir
+    #error checking for not having a path at all.
+    if(dir == None):
+        print('''Failed to export,
+reason:  You have to specify a path to export using mode 2 !!!''')
+    else:
+        print('file exporting to "' + dir + '" ...')
+# local folder
+else: 
+    dir = './'
+
+print(dir)
+encrypt_data("test.json", "testing.key", dir)
--- error_detection/unloader.py
+++ error_detection/unloader.py
@@ -1,21 +1,19 @@
 from cryptography.fernet import Fernet
+from common import load_key
 import os
 
-def load_key(key_file):
-    return open(key_file, "rb").read()
+def decrypt(keyfile)
+    os.system('./decode-crc32 encoded_data.txt > decoded_data.txt')
 
-os.system('./decode-crc32 encoded_data.txt > decoded_data.txt')
+    with open('decoded_data.txt', 'rb') as file:
+        data = file.read()
 
-with open('decoded_data.txt', 'rb') as file:
-    data = file.read()
+    key = load_key(keyfile)
+    f = Fernet(key)
+    decrypted_msg = f.decrypt(data)
 
+    encrypt_output_file = open("data.json", "wb")
+    encrypt_output_file.write(decrypted_msg)
+    encrypt_output_file.close()
 
-keyfile = "testing.key"
-key = load_key(keyfile)
-f = Fernet(key)
-decrypted_msg = f.decrypt(data)
-
-print(decrypted_msg)
-encrypt_output_file = open("data.json", "wb")
-encrypt_output_file.write(decrypted_msg)
-encrypt_output_file.close()
+decrypt("testing.key")
